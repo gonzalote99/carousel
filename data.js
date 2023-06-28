1abce259d204e27121cee57319b4b2c23a1b6d66
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-06-28 18:14:08 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const people = [
+  {
+    img:"https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",
+    name: "name 1 ",
+    job: "job 1 ",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+  },
+  {
+    img:"https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",
+    name: "name 2 ",
+    job: "job 2",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+  },
+  {
+    img:"https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",
+    name: "name 3",
+    job: "job 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit"
+  },
+];
+
+export default people;
